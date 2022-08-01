09e093caa11c4c78259c01d83e174cb8474ab8ad
Jérémy Laynaud <jeremy.l@objectif54.fr>
2022-08-01 15:08:19 +0200

    reparation redirection page login

--- src/main/java/com/obj54/securingweb/WebSecurityConfig.java
+++ src/main/java/com/obj54/securingweb/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.obj54.securingweb;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -17,11 +18,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers("/", "/home").permitAll()
-				.anyRequest().authenticated()
+				.antMatchers("/", "/home","/static/css/**","/static/img/**").permitAll()
+				
 				.and()
 			.formLogin()
-				.loginPage("/login")
+				.loginPage("/home/login")
 				.permitAll()
 				.and()
 			.logout()
